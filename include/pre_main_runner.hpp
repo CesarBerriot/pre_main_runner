5ad12cbb5bcc6be1ba6b94d5b8e3303ce2c88a8a
unknown <berriot.cesar.com>
2025-09-10 15:50:28 +0200

    bugfix

--- include/pre_main_runner.hpp
+++ include/pre_main_runner.hpp
@@ -5,11 +5,10 @@
 #define RUN_PRE_MAIN_2(counter, ...) RUN_PRE_MAIN_3(counter, __VA_ARGS__)
 
 #define RUN_PRE_MAIN_3(counter, ...)	\
-namespace pre_main_runner_##counter					\
-{	static class runner								\
-	{	public:										\
-			runner(void)							\
-			{	__VA_ARGS__							\
-			}										\
-	} runner;										\
-}
+int pre_main_runner_unused_##counter =	\
+	[]									\
+	{	do								\
+		{	__VA_ARGS__					\
+		} while(false);					\
+		return 0;						\
+	}();
